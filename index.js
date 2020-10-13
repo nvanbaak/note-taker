b2a930ec7a23ee698f051ff8779da1f7c9bd4d8f
Nikolai <nvanbaak@gmail.com>
2020-10-13 08:29:44 -0700

    databse setup and write functions

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // This file handles the server and routing
 
-// Express boilerplate
+// Dependencies
 const express = require('express');
+const fs = require("fs")
+
+// ======================================
+//          EXPRESS BOILERPLATE
+// ======================================
 
 const app = express();
-const PORT = 9164;
+const PORT = process.env.PORT || 9164;
 
 // Gives us more options for POST requests
 app.use(express.urlencoded({ extended: true }));
@@ -16,19 +21,54 @@ app.listen(PORT, function() {
 });
 
 
+// =======================================
+//               Routing
+// =======================================
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
+
 
 app.get("/notes", function (req, res) {
     res.sendFile(__dirname + "/public/notes.html");
 });
 
+app.get("/api/notes", function(req, res) {
+    res.sendFile(__dirname + "/public/db.json");
+})
+
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 
+// ===============================
+//        JSON READ / WRITE
+// ===============================
 
+// The notes database is an array of objects each with a title key and a content key
 
+// Defines path to notes
+const dbPath = __dirname + "/public/db.json";
 
+// create database reference
+let notesDB = []
+
+// If the notes db doesn't exist, we make it
+if (!fs.existsSync(dbPath)) {
+    notesDB = [{
+        title:"Welcome!",
+        content:"Check out the readme for information on app functionality1"
+    }];
+
+    storeDB();
+}
+
+// This function stores the database by writing it to db.json
+function storeDB() {
+    fs.writeFile(dbPath,JSON.stringify(notesDB),"utf8",err=>{if (err) throw err});
+}
 
 
 
